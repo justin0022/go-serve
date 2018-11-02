3b7a2dcb36a6f41fb66030912ba8fb114e6afdd9
Justin Lee <justinlee0022@gmail.com>
2018-11-02 10:03:20 -0700

    Small modifications

--- server.go
+++ server.go
@@ -5,9 +5,14 @@ import (
 	"net/http"
 )
 
+func githubHandler() {
+
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
+	http.HandleFunc("/github/", githubHandler)
 
 	log.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
