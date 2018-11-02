70587f8c935714f18e134d3207ed8e3eddc08727
Justin Lee <justinlee0022@gmail.com>
2018-11-02 10:14:21 -0700

    ohhh first package import

--- server.go
+++ server.go
@@ -3,16 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func githubHandler() {
+func githubHandler(w http.ResponseWriter, req *http.Request) {
 
 }
 
 func main() {
+	router := mux.NewRouter()
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
-	http.HandleFunc("/github/", githubHandler)
+	router.HandleFunc("/github/", githubHandler).Methods("GET")
 
 	log.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
