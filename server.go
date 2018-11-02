d5c660dbe01d3ba227ab03c5b7421c26400d9884
Justin Lee <justinlee0022@gmail.com>
2018-11-02 10:49:21 -0700

    Working api call to github

--- server.go
+++ server.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func githubHandler(w http.ResponseWriter, req *http.Request) {
-
+func GithubHandler(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	w.WriteHeader(http.StatusOK)
+	response, _ := http.Get("https://api.github.com/users/" + params["username"])
+	fmt.Println(response)
 }
 
 func main() {
 	router := mux.NewRouter()
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
-	router.HandleFunc("/github/", githubHandler).Methods("GET")
+	router.Handle("/", fs)
+	router.HandleFunc("/github/{username}", GithubHandler)
 
 	log.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
 }
